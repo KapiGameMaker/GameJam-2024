2700d4172afc693a21e031e836be1f33bf319e9d
Rain Maker <108649582+KapiGameMaker@users.noreply.github.com>
2024-01-26 19:03:07 +0700

    setting

---
+++ Assets/Scripts/dragable.cs
@@ -0,0 +1,36 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.EventSystems;
+
+public class dragable : MonoBehaviour, IBeginDragHandler, IDragHandler, IEndDragHandler
+{
+    public Image image;
+    [HideInInspector] public Transform parentAfterDrag;
+
+    private void Awake()
+    {
+        image = GetComponent<Image>();
+    }
+
+    public void OnBeginDrag(PointerEventData eventData)
+    {
+        Debug.Log("Begin drag");
+        parentAfterDrag = transform.parent;
+        transform.SetParent(transform.root);
+        transform.SetAsLastSibling();
+        image.raycastTarget = false;
+    }
+    public void OnDrag(PointerEventData eventData)
+    {
+        Debug.Log("Dragging");
+        transform.position = Input.mousePosition;
+    }
+    public void OnEndDrag(PointerEventData eventData)
+    {
+        Debug.Log("End drag");
+        transform.SetParent(parentAfterDrag);
+        image.raycastTarget = true;
+    }
+}
