a27f70ff7f3636be8461001a71c201c8beb89615
Rain Maker <108649582+KapiGameMaker@users.noreply.github.com>
2024-01-26 20:37:29 +0700

    Set Up Drag n Drop

--- Assets/Scripts/dragable.cs
+++ Assets/Scripts/dragable.cs
@@ -1,36 +1,64 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.UI;
 using UnityEngine.EventSystems;
 
-public class dragable : MonoBehaviour, IBeginDragHandler, IDragHandler, IEndDragHandler
+public class dragable : MonoBehaviour
 {
-    public Image image;
-    [HideInInspector] public Transform parentAfterDrag;
+    public LayerMask m_DragLayers;
 
-    private void Awake()
-    {
-        image = GetComponent<Image>();
-    }
+    [Range(0.0f, 100.0f)]
+    public float m_Damping = 1.0f;
 
-    public void OnBeginDrag(PointerEventData eventData)
+    [Range(0.0f, 100.0f)]
+    public float m_Frequency = 5.0f;
+
+    public bool m_DrawDragLine = true;
+    public Color m_Color = Color.cyan;
+
+    private TargetJoint2D m_TargetJoint;
+
+    void Update()
     {
-        Debug.Log("Begin drag");
-        parentAfterDrag = transform.parent;
-        transform.SetParent(transform.root);
-        transform.SetAsLastSibling();
-        image.raycastTarget = false;
-    }
-    public void OnDrag(PointerEventData eventData)
-    {
-        Debug.Log("Dragging");
-        transform.position = Input.mousePosition;
-    }
-    public void OnEndDrag(PointerEventData eventData)
-    {
-        Debug.Log("End drag");
-        transform.SetParent(parentAfterDrag);
-        image.raycastTarget = true;
+        // Calculate the world position for the mouse.
+        var worldPos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+
+        if (Input.GetMouseButtonDown(0))
+        {
+            // Fetch the first collider.
+            // NOTE: We could do this for multiple colliders.
+            var collider = Physics2D.OverlapPoint(worldPos, m_DragLayers);
+            if (!collider)
+                return;
+
+            // Fetch the collider body.
+            var body = collider.attachedRigidbody;
+            if (!body)
+                return;
+
+            // Add a target joint to the Rigidbody2D GameObject.
+            m_TargetJoint = body.gameObject.AddComponent<TargetJoint2D>();
+            m_TargetJoint.dampingRatio = m_Damping;
+            m_TargetJoint.frequency = m_Frequency;
+
+            // Attach the anchor to the local-point where we clicked.
+            m_TargetJoint.anchor = m_TargetJoint.transform.InverseTransformPoint(worldPos);
+        }
+        else if (Input.GetMouseButtonUp(0))
+        {
+            Destroy(m_TargetJoint);
+            m_TargetJoint = null;
+            return;
+        }
+
+        // Update the joint target.
+        if (m_TargetJoint)
+        {
+            m_TargetJoint.target = worldPos;
+
+            // Draw the line between the target and the joint anchor.
+            if (m_DrawDragLine)
+                Debug.DrawLine(m_TargetJoint.transform.TransformPoint(m_TargetJoint.anchor), worldPos, m_Color);
+        }
     }
 }
