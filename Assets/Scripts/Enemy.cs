2c901cfe4ef23c06269731c3d43f1ba1f81ecf12
nano1412 <54862667+nano1412@users.noreply.github.com>
2024-01-26 23:12:13 +0700

    spawner can spawn multiple time now

--- Assets/Scripts/Enemy.cs
+++ Assets/Scripts/Enemy.cs
@@ -4,11 +4,12 @@ using UnityEngine;
 
 public class Enemy : MonoBehaviour
 {
-    public GameObject enemy;
     public Rigidbody2D enemyRigidbody;
     public float speed;
     public float speed_cap;
+    public float lifespan;
 
+    private float currentTime = 0f;
     // Start is called before the first frame update
     void Start()
     {
@@ -19,5 +20,12 @@ public class Enemy : MonoBehaviour
     void Update()
     {
         enemyRigidbody.AddForce(Vector2.ClampMagnitude(new Vector2(speed, 0), speed_cap));
+
+        currentTime += Time.deltaTime;
+        if (currentTime >= lifespan)
+        {
+            Destroy(gameObject);
+            currentTime = 0f;
+        }
     }
 }
--- Assets/Scripts/Enemy_Spawner.cs
+++ Assets/Scripts/Enemy_Spawner.cs
@@ -2,13 +2,16 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class BirdSpawner : MonoBehaviour
+public class Enemy_Spawner : MonoBehaviour
 {
     public float spawn_rate;
     public GameObject bird_prefab;
 
     public float speed;
     public float speed_cap;
+    public float lifespan;
+
+    private float currentTime = 0f;
 
     // Start is called before the first frame update
     void Start()
@@ -17,12 +20,26 @@ public class BirdSpawner : MonoBehaviour
         Enemy set = bird.GetComponent<Enemy>();
         set.speed = speed;
         set.speed_cap = speed_cap;
+        set.lifespan = lifespan;
 
     }
 
     // Update is called once per frame
     void Update()
     {
+        currentTime += Time.deltaTime;
+        if (currentTime >= spawn_rate)
+        {
+            Spawn_Enemy();
+            currentTime = 0f;
+        }
+    }
 
+    void Spawn_Enemy()
+    {
+        GameObject bird = Instantiate(bird_prefab, this.transform);
+        Enemy set = bird.GetComponent<Enemy>();
+        set.speed = speed;
+        set.speed_cap = speed_cap;
     }
 }
